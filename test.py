4900da1ccccbde382f488131fce5016c12b93329
Yakov-gel <56504061+Yakov-gel@users.noreply.github.com>
2020-06-25 19:13:44 +0300

    test 2

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ cap = cv2.VideoCapture(0)
 while (1):
     _, image = cap.read()
     cv2.imshow("show", image)
+    can = cv2.Canny(image, 25, 255, None, None, 1)
+    cv2.imshow("can", can)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
